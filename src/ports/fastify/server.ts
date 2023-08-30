055f39847e7b42ddb52dd54d49cdb25f24c4bb6f
PHAM DUY TIEN <duytien@MacBook-Pro-cua-PHAM.local>
2023-08-31 00:59:26 +0700

    feat: prepare fastify

---
+++ src/ports/fastify/server.ts
@@ -0,0 +1,10 @@
+import * as configurationProvider from '@libraries/configuration-provider';
+import { AddressInfo } from 'net';
+import configurationSchema from '@config';
+import fastify from 'fastify';
+
+// async function startWebServer(): Promise<AddressInfo> {
+//   configurationProvider.initializeAndValidate(configurationSchema);
+
+//   const fastifyApp = fastify();
+// }
